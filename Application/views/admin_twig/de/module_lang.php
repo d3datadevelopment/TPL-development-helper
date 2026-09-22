31e6b1381b4f39e47b4eb3cff3fc82661b5306cc
Daniel Seifert <git@daniel-seifert.com>
2026-09-22 10:13:38 +0200

    add option translations for Smarty and Twig based themes

---
+++ Application/views/admin_twig/de/module_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * https://www.d3data.de
+ *
+ * @copyright (C) D3 Data Development (Inh. Thomas Dartsch)
+ * @author    D3 Data Development - Daniel Seifert <info@shopmodule.com>
+ * @link      https://www.oxidmodule.com
+ */
+
+declare(strict_types=1);
+
+// @codeCoverageIgnoreStart
+$aLang = include __DIR__."/../../de/translations.php";
+// @codeCoverageIgnoreEnd
--- Application/views/de/translations.php
+++ Application/views/de/translations.php
@@ -1,10 +1,8 @@
 <?php
 
 /**
- * Copyright (c) D3 Data Development (Inh. Thomas Dartsch)
- *
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  *
  * https://www.d3data.de
  *
@@ -13,12 +11,9 @@
  * @link      https://www.oxidmodule.com
  */
 
-$sLangName = "Deutsch";
+declare(strict_types=1);
 
-// -------------------------------
-// RESOURCE IDENTITFIER = STRING
-// -------------------------------
-$aLang = [
+return [
     //Navigation
     'charset'                                                   => 'UTF-8',
 
